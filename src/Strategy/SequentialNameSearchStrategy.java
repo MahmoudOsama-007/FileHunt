2bb491cc4e12d80ecdba939f478367c4ce4cf738
Maxmo <Lap@Maxmo>
2025-04-22 00:46:25 +0200

    SequentialNameSearchStrategy

--- src/Strategy/SequentialNameSearchStrategy.java
+++ src/Strategy/SequentialNameSearchStrategy.java
@@ -1,12 +1,43 @@
 package Strategy;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SequentialNameSearchStrategy implements SearchStrategy {
     @Override
     public List<File> search(String query, String path) {
-        System.out.println("Dummy Search");
-        return List.of();
+        List<File> matchedFiles = new ArrayList<>();
+        File root = new File(path);
+
+        if (root.exists() && root.isDirectory()) {
+            searchDirectory(root,query ,matchedFiles);
+        } else {
+            System.err.println("Invalid path: " + path);
+        }
+
+        return matchedFiles;
+    }
+
+    private void searchDirectory(File dir,String query,List<File> matchedFiles) {
+        File[] files = dir.listFiles();
+
+        if (files == null) return;
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                searchDirectory(file,query,matchedFiles);  // Recurse into subdirectories
+            } else {
+                if (filenameMatches(file,query)) {
+                    System.out.println(file.getAbsolutePath()); // Test
+                    matchedFiles.add(file);
+                }
+            }
+        }
+    }
+    private boolean filenameMatches(File file,String query) {
+        String fileNameLower = file.getName().toLowerCase();
+        String queryLower = query.toLowerCase();
+        return fileNameLower.contains(queryLower);
     }
 }
