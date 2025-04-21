ae726d3bd98e8efc8d29568105b4f8a7b26b4d0d
lab@maxmo <lab@example.com>
2025-04-21 20:18:04 +0200

    Update Main: Dashbord for choose Search Algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import Strategy.*;
+
+import java.io.File;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        FileSearchContext context = new FileSearchContext();
+
+        while (true) {
+            System.out.println("Choose a search strategy:");
+            System.out.println("1. Find files containing a word (Sequential)");
+            System.out.println("2. Find files containing a word (Parallel)");
+            System.out.println("3. Find file by name (Sequential)");
+            System.out.println("4. Find file by name (Parallel)");
+            System.out.println("5. Exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            if (choice == 5) {
+                System.out.println("Exiting...");
+                break;
+            }
+
+            System.out.print("Enter search query (word or filename): ");
+            String query = scanner.nextLine();
+
+            System.out.print("Enter Drive to search: ");
+            String path = scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    context.setStrategy(new SequentialWordSearchStrategy());
+                    break;
+                case 2:
+                    context.setStrategy(new ParallelWordSearchStrategy());
+                    break;
+                case 3:
+                    context.setStrategy(new SequentialNameSearchStrategy());
+                    break;
+                case 4:
+                    context.setStrategy(new ParallelNameSearchStrategy());
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    continue;
+            }
+
+            List<File> results = context.search(query, path);
+
+
+            if (results.isEmpty()) {
+                System.out.println("No files found.");
+            } else {
+                System.out.println("Found files:");
+                for (File file : results) {
+                    System.out.println(file.getAbsolutePath());
+                }
+            }
+
+        }
+
+        scanner.close();
     }
 }
